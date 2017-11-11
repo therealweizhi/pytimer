59651c87847d118bcf37c291a8dda69c6da0ad2e
Inviro <prelusiondilemma@gmail.com>
2017-11-10 19:12:05 -0800

    Add files via upload
    Changed the Timer into an object, and used the UI as attributes to the Timer object.
    Added input validation to ensure that inserted characters are numeric.

--- pytimer.py
+++ pytimer.py
@@ -1,27 +1,44 @@
-# Import necessary modules
-import time
-from tkinter import *
+from tkinter import *  # Python 3
+from time import sleep as slp # sleep imported as slp
 
-# Define the timer
-def timer():
-    seconds = e1.get()
-    time.sleep(int(seconds))
-    l1.destroy()
-    e1.destroy()
-    b1.destroy()
-    l2.pack(side = TOP, pady = 25)
-    b2.pack(side = BOTTOM, pady = 5)
+class Timer(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
 
-# Create the GUI
-win = Tk()
-win.title('pytimer')
-win.geometry('375x100')
-l1 = Label(win, text = 'How long do you want to set the timer for in seconds?')
-l2 = Label(win, text = 'Timer finished.')
-e1 = Entry(win)
-b1 = Button(win, text = 'Start Timer', command = timer)
-b2 = Button(win, text = 'OK', command = quit)
-l1.pack(side = TOP, pady = 5)
-e1.pack(side = TOP)
-b1.pack(side = BOTTOM, pady = 5)
-win.mainloop()
+        def timer(): # Define the timer
+            seconds = Timer.entry.get()
+            slp(int(seconds)) # sleeps for seconds
+
+            self.label.destroy()
+            self.entry.destroy()
+            self.button.destroy()
+
+            self.label2.pack(side = TOP, pady = 25)
+            self.button2.pack(side = BOTTOM, pady = 5)
+
+        vcmd = (self.register(self.onValidate), '%S') # %S is the inserted string
+        Timer.label = Label(self, text = 'How long do you want to set the timer for in seconds?')
+        Timer.entry = Entry(self, validate = "key", validatecommand=vcmd)
+        Timer.button = Button(self, text = 'Start Timer', command = timer)
+
+        Timer.label.pack(side = "top")
+        Timer.entry.pack(side = "top")
+        Timer.button.pack(side = "top")
+
+        Timer.label2 = Label(self, text = 'Timer finished.')
+        Timer.button2 = Button(self, text = 'ok', command = quit)
+
+    def onValidate(self, input): # Used to validate the inserted text on entry
+        if (input.isnumeric()):
+            return True
+        else:
+            self.bell()
+            return False
+def main():
+    win = Tk() # Frame used in Timer
+    win.title('pytimer')
+    win.geometry('375x100')
+    Timer(win).pack()
+    win.mainloop()
+
+main()
