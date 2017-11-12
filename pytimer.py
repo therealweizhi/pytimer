d46fa156c07f1c0831acf220dbac7e652872022f
Inviro <prelusiondilemma@gmail.com>
2017-11-11 23:42:18 -0800

    Add files via upload
    Removed an extra variable declaration, and changed the countdown timer to hh:mm:ss instead of just mm:ss
    Also tweaked some variables in the GUI to look more aesthetically pleasing

--- pytimer.py
+++ pytimer.py
@@ -5,10 +5,9 @@ except ImportError:
 from time import sleep as slp # sleep imported as slp
 
 class Timer(Frame):
-    timeleft = ""
     def __init__(self, parent):
         Frame.__init__(self, parent)
-        def myCountDown():
+        def countDown():
             mySeconds = Timer.entry.get()
             mySeconds = mySeconds if mySeconds else 0 # Checks if there is no input
             timeleft = StringVar()
@@ -16,33 +15,34 @@ class Timer(Frame):
             self.label.destroy()
             self.entry.destroy()
             self.button.destroy()
-            self.label3 = Label(self, text = "Time Remaining")
+            self.label3 = Label(self, text = "Time Remaining:")
             self.label4 = Label(self, textvariable = timeleft)
             self.label3.pack(side = "top", pady = 25)
             self.label4.pack(side = "top")
 
             # for each second from mySeconds until -1
             for t in range(int(mySeconds), 0, -1):
-                # format as 2 digit integers, fills with zero to the left
-                # divmod() gives minutes, seconds
-                sf = "{:02d}:{:02d}".format(*divmod(t, 60))
-                timeleft.set(sf)
+                # formatted as two digit numbers with zeros as fill
+                # divmod() gives minutes, seconds and then hours, minutes
+                m, s = divmod(t, 60)
+                h, m = divmod(m, 60)
+                timeleft.set("%02d:%02d:%02d" % (h, m, s))
                 self.update()
                 slp(1)
 
             self.label3.destroy()
             self.label4.destroy()
-            self.label2.pack(side = TOP, pady = 25)
+            self.label2.pack(side = TOP, pady = 20)
             self.button2.pack(side = BOTTOM, pady = 0) ## pady5
 
         vcmd = (self.register(self.onValidate), '%S') # %S is the inserted string
         Timer.label = Label(self, text = 'How long do you want to set the timer for in seconds?')
-        Timer.entry = Entry(self, validate = "key", validatecommand=vcmd)
-        Timer.button = Button(self, text = 'Start Timer', command = myCountDown)
+        Timer.entry = Entry(self, validate = "key", validatecommand = vcmd)
+        Timer.button = Button(self, text = 'Start Timer', command = countDown)
 
         Timer.label.pack(side = "top")
-        Timer.entry.pack(side = "top")
-        Timer.button.pack(side = "top")
+        Timer.entry.pack(side = "top", pady = 5)
+        Timer.button.pack(side = "top", pady = 3)
 
         Timer.label2 = Label(self, text = 'Timer finished.')
         Timer.button2 = Button(self, text = 'ok', command = quit)
@@ -53,6 +53,7 @@ class Timer(Frame):
         else:
             self.bell()
             return False
+
 def main():
     win = Tk() # Frame used in Timer
     win.title('pytimer')
